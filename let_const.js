95d9e180b899c99bc9577ab22f726a06f7a1ed88
Antonio Jackson <antoniojackson@gmail.com>
2020-11-12 15:01:00 -0700

    Initial commit with let & const examples

---
+++ let_const.js
@@ -0,0 +1,17 @@
+var myName = 'Antonio';
+console.log(myName);
+
+myName = 'Tony';
+console.log(myName);
+
+let hisName = 'Max';
+console.log(hisName);
+
+hisName = 'Manu';
+console.log(hisName);
+
+const herName = 'Karen';
+console.log(herName);
+
+herName = 'Katherine';
+console.log(herName);
