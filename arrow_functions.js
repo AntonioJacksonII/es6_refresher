bb69d8a024249c4281e16ddb83ab10d7292b8ff6
Antonio Jackson <antoniojackson@gmail.com>
2020-11-12 16:32:32 -0700

    Adding arrow functions examples

---
+++ arrow_functions.js
@@ -0,0 +1,21 @@
+function printMyName(name, age) {
+    console.log(name, age);
+}
+
+printMyName('Antonio', 32);
+
+const printName = (name, age) => {
+    console.log(name, age);
+}
+
+printName('Tony', 32);
+
+const multiply = (number) => {
+    return number * 2
+}
+
+console.log(multiply(2));
+
+const double = number => number * 2;
+
+console.log(double(2));
