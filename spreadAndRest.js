5658163acb6e5f37c72eebddb04988c59942ea3f
Antonio Jackson <antoniojackson@gmail.com>
2020-11-12 18:16:51 -0700

    Adding spread and rest examples

---
+++ spreadAndRest.js
@@ -0,0 +1,21 @@
+
+// Spread
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4];
+
+console.log(newNumbers);
+
+const person = {
+    name: 'Tony'
+};
+
+const newPerson = {
+    ...person,
+    age: '32'
+};
+console.log(newPerson);
+
+// Rest
+const filter = (...args) => args.filter(el => el === 1);
+
+console.log(filter(1, 2, 3));
